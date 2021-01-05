202a60c2491f53d8f35e1f7d408d42888224ace9
刘书豪 <liushuhao@cadg.cn>
2021-01-05 11:26:05 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <div style="height: 500px; width: 500px; border: 1px solid red; position: relative;">
+      <vue-draggable-resizable :itemdata ='item'  v-for="(item, index) in datao" :key = 'index' :w="400" :h="400" :parent="true" :min-width="200" :min-height="200" :ref="'sss'+ index">
+        <p>vue-draggable-resizable</p>
+      </vue-draggable-resizable>
+    </div>
+  </div>
+</template>
+
+<script>
+import VueDraggableResizable from './components/vue-draggable-resizable'
+import './components/vue-draggable-resizable.css'
+
+export default {
+  name: 'app',
+  components: {
+    VueDraggableResizable
+  },
+  mounted () {
+    this.$on('contextmenu', (data) => {
+      console.log(data)
+    })
+  },
+  data: function () {
+    return {
+      datao: [
+        {
+          id: 1
+        },
+        {
+          id: 2
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+  .vdr {
+    border: 1px dashed black;
+  }
+</style>
